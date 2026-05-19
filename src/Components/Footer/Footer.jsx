fa57e404ab3df71ba3a7b8ef412f45e570be2c93
Damian Wujec <wujecdamian12@gmail.com>
2026-05-19 13:53:02 +0200

    refactor: add copy sign and brand name to footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import styles from "./Footer.module.css";
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <p>I am footer!</p>
+      <p>Rowan Calder &copy;</p>
     </footer>
   );
 };
