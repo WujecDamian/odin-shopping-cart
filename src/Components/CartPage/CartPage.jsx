ad502a9efb479a1abdc4931e77dc311b92b04de6
Damian Wujec <wujecdamian12@gmail.com>
2026-05-19 10:50:54 +0200

    feat: add 3 main pages

---
+++ src/Components/CartPage/CartPage.jsx
@@ -0,0 +1,9 @@
+const CartPage = () => {
+  return (
+    <section className="cart">
+      <h1>Cart</h1>
+    </section>
+  );
+};
+
+export default CartPage;
---
+++ src/Components/ShopPage/ShopPage.jsx
@@ -0,0 +1,9 @@
+const ShopPage = () => {
+  return (
+    <section className="shop">
+      <h1>Shop</h1>
+    </section>
+  );
+};
+
+export default ShopPage;
