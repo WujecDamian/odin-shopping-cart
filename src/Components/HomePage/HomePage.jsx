d4761c44b8a595d37b1d8229e72b057889cc6d2c
Damian Wujec <wujecdamian12@gmail.com>
2026-05-19 13:45:11 +0200

    feat: put content into HomePage and connect styles

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -1,7 +1,22 @@
+import styles from "./HomePage.module.css";
+import { Link } from "react-router-dom";
+import homeImage from "../../assets/images/homepage_imgLarge.jpeg";
+
 const HomePage = () => {
   return (
-    <section className="home">
-      <h1>Home</h1>
+    <section className={styles.home}>
+      <section className={styles.home__left}>
+        <h1>Rowan Calder</h1>
+        <h2>
+          Timeless <span>essentials</span> for the modern life.
+        </h2>
+        <Link to="../shop" id="link" className={styles.link}>
+          Discover Rowan Calder
+        </Link>
+      </section>
+      <section className={styles.home__right}>
+        <img src={homeImage} alt="Woman in Rowan Calder lingerie" />
+      </section>
     </section>
   );
 };
