9393d4c7a1175c51f0fbe418e2d182256abbfb28
Damian Wujec <wujecdamian12@gmail.com>
2026-05-19 12:19:16 +0200

    style: make width alignment for better layout

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,6 +1,8 @@
+import styles from "./Footer.module.css";
+
 const Footer = () => {
   return (
-    <footer className="footer">
+    <footer className={styles.footer}>
       <h3>I am footer!</h3>
     </footer>
   );
--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,12 +1,13 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "../NavBar/NavBar";
 import Footer from "../Footer/Footer";
+import styles from "./Layout.module.css";
 
 const Layout = () => {
   return (
     <>
       <NavBar></NavBar>
-      <main className="main">
+      <main className={styles.main}>
         <Outlet />
       </main>
       <Footer></Footer>
