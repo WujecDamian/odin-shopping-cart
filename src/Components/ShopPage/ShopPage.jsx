2018c093faa49bf007304a0e408b285e6dde1c63
Damian Wujec <wujecdamian12@gmail.com>
2026-05-19 21:17:29 +0200

    feat: implement ShopPage component with product fetching and styling

--- src/Components/ShopPage/ShopPage.jsx
+++ src/Components/ShopPage/ShopPage.jsx
@@ -1,7 +1,25 @@
+import { useEffect, useState } from "react";
+import ItemCard from "./ItemCard/ItemCard";
+import styles from "./ShopPage.module.css";
+
 const ShopPage = () => {
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products?sort=desc&limit=6")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+
   return (
-    <section className="shop">
+    <section className={styles.shop}>
       <h1>Shop</h1>
+      <section className={styles.products}>
+        {products.map((product) => (
+          <ItemCard props={product} key={product.id}></ItemCard>
+        ))}
+      </section>
     </section>
   );
 };
