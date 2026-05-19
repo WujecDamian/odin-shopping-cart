4d6d00b27e4cf9b27601f4c35f95c35f28948faf
Damian Wujec <wujecdamian12@gmail.com>
2026-05-19 11:07:32 +0200

    feat(routes): add routes to shop & cart

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,12 +1,23 @@
 import HomePage from "./Components/HomePage/HomePage";
 import CartPage from "./Components/CartPage/CartPage";
 import ShopPage from "./Components/ShopPage/ShopPage";
+import ErrorElement from "./Components/ErrorElement/ErrorElement";
 
 const routes = [
   {
     path: "/",
     element: <HomePage></HomePage>,
-    errorElement: <ErrorPage></ErrorPage>,
+    errorElement: <ErrorElement></ErrorElement>,
+  },
+  {
+    path: "shop",
+    element: <ShopPage></ShopPage>,
+    errorElement: <ErrorElement></ErrorElement>,
+  },
+  {
+    path: "cart",
+    element: <CartPage></CartPage>,
+    errorElement: <ErrorElement></ErrorElement>,
   },
 ];
 
