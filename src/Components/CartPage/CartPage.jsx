9c4c934d82c68be56187973e6d5aaf1eb9b122c2
Damian Wujec <wujecdamian12@gmail.com>
2026-05-20 11:20:07 +0200

    feat: implement CartPage component with context and styling

--- src/Components/CartPage/CartPage.jsx
+++ src/Components/CartPage/CartPage.jsx
@@ -1,7 +1,22 @@
+import { useContext } from "react";
+import styles from "./CartPage.module.css";
+import { CartContext } from "../../Context/CartContext";
+import CartItem from "./CartItem/CartItem";
+
 const CartPage = () => {
+  const { cart } = useContext(CartContext);
+  console.table({ cart });
+  const listProducts = cart.map((product) => (
+    <CartItem props={product}></CartItem>
+  ));
+
   return (
-    <section className="cart">
+    <section className={styles.cart}>
       <h1>Cart</h1>
+      <section className={styles.cart__left}>
+        <ul>{listProducts}</ul>
+      </section>
+      <section className={styles.cart__right}></section>
     </section>
   );
 };
